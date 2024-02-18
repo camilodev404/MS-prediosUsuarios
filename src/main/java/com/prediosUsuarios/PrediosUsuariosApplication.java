dd8dbbe6affe95d5b640f992fef69bd774381145
CamiloDev404 <crosti1826@gmail.com>
2024-02-17 20:40:08 -0500

    First Commit with Principal Configuration

---
+++ src/main/java/com/prediosUsuarios/PrediosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.prediosUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrediosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrediosUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prediosUsuarios/PrediosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prediosUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrediosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
